9e9122465ae1b6260813fabffe40bf5a00a57bad
avikola <avishkar1995@gmail.com>
2020-01-15 18:11:13 -0800

    Initial

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <>
+    <SEO title="Software Developer" />
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
+  </>
+)
+
+export default IndexPage
